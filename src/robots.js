d3d29b44dcaaab34253a255ed931dc3e0ea1bdc6
sunbetkapper@gmail.com <napvital@gmail.com>
2020-04-24 21:12:34 +0200

    added all component and functionality without redux

---
+++ src/robots.js
@@ -0,0 +1,53 @@
+export const robots =[{
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+},{
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+},{
+    id: 3,
+    name: 'Clementine Bauch',
+    username: "Samantha",
+    email: 'Nathan@senia.tv'
+},
+{
+    id:4,
+    name: 'Patricia Lebsak',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+},{
+    id:5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+},{
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+},{
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skilles',
+    email: 'Trelly.Hoeger@billy.biz'
+},{
+    id: 8,
+    name: 'Nicolaz Rudzvilg B',
+    username: 'Binnie_Maxim',
+    email: 'Tellysherwood@roasmond.me'
+},{
+    id: 9,
+    name: 'Glena Rugata',
+    username: 'Shark',
+    email:'Delphin@musa.to'
+},{
+    id: 10,
+    name: 'Clementina Dubuque',
+    username:'Miriam.Stanfork',
+    email:'Rey.Palmerstone@karina.flu'
+}
+]
