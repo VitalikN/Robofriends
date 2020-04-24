fff33efd0695c82c0cec269a7744eab31c0441f2
sunbetkapper@gmail.com <napvital@gmail.com>
2020-04-24 22:17:26 +0200

    added redux to the app, created searchRobots reducer and searchField action

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots } from './redux/reducer';
 
+const store = createStore(searchRobots);
 
-ReactDOM.render(<App />, document.getElementById('root')
+ReactDOM.render( 
+<Provider store = {store} >
+   <App />
+</Provider>
+ , document.getElementById('root')
 );
 
 
