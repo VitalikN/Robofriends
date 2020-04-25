b9be56fafaf8ca64a88ef6512a585e051ba0953c
sunbetkapper@gmail.com <napvital@gmail.com>
2020-04-25 13:41:48 +0200

    switched the API request to redxux, implemented asynchron redux with redux-thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
  import React  from 'react';
+ import { connect } from 'react-redux';
  import CardList from '../components/CardList';
  import SearchBox from '../components/SearchBox';
  import Scroll from '../components/Scroll';
@@ -6,37 +7,44 @@
  import './App.css';
 
 
+ import { setSearchField, requestRobots } from '../redux/actions';
+
+
+  const mapStateToProps = state => ({
+      searchField: state.searchRobots.searchField,
+      robots: state.requestRobots.robots,
+      isPending: state.requestRobots.isPending,
+      error: state.requestRobots.error
+  });
+
+  const mapDispatchToProps = (dispatch) => {
+      return {
+          onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+          onRequestRobots: () => dispatch(requestRobots())
+        }
+  };
+
 
 
  class App extends React.Component {
-     constructor()  {
-         super();
-         this.state = {
-             robots: [],
-             searchfield: ''
-         }
-     }
+
+    
 
      componentDidMount() {
-         fetch('https://jsonplaceholder.typicode.com/users')
-         .then(response=> response.json())
-         .then(users=> this.setState({ robots: users}));
+         this.props.onRequestRobots();
      }
 
-     onSearchChange = (event) => {
-         this.setState({searchfield: event.target.value});
-     }
      render() {
-         const {robots,searchfield} = this.state;
+         const {robots, isPending, searchField, onSearchChange} = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
           });
-          return !robots.length ?
+          return isPending ?
           <h1>Loading</h1> :
           ( 
              <div className = " tc" >
               <h1 className="f1"> Your Robofriends </h1> 
-              <SearchBox searchChange={this.onSearchChange} />
+              <SearchBox searchChange={onSearchChange} />
               <Scroll>
                 <ErrorBoundry>
                  <CardList robots = {filteredRobots }/>
@@ -49,4 +57,6 @@
 
  }
 
- export default App;
+
+
+ export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import  thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
 
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { searchRobots } from './redux/reducer';
+import { searchRobots, requestRobots } from './redux/reducer';
 
-const store = createStore(searchRobots);
+
+const logger = createLogger();
+const rootReducer = combineReducers({
+    searchRobots,
+    requestRobots
+})
+const store = 
+createStore(rootReducer, 
+    applyMiddleware(thunkMiddleware, logger)
+    );
+
 
 ReactDOM.render( 
 <Provider store = {store} >
