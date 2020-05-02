ea3699d793e4b73468cafe186afbe54dabe88096
sunbetkapper@gmail.com <napvital@gmail.com>
2020-05-02 20:28:48 +0200

    added label for input, changed alt attributes for photos of robots;

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchField, searchChange}) => {
     console.log('searchbox')
     return ( 
     <div className = 'pa2' >
+        <label> </label>
         <input className = 'pa3 ba b--green bg-lightest-blue'
         type = 'search'
         placeholder = 'search robots' 
