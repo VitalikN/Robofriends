f1e9dfd196787fbba10d0505ffbc858fcc281411
sunbetkapper@gmail.com <napvital@gmail.com>
2020-05-02 20:32:08 +0200

    a

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,10 +4,12 @@ const SearchBox = ({searchField, searchChange}) => {
     console.log('searchbox')
     return ( 
     <div className = 'pa2' >
-        <label> </label>
+        <label for="robot"> search robots </label>
         <input className = 'pa3 ba b--green bg-lightest-blue'
         type = 'search'
-        placeholder = 'search robots' 
+        name='robot'
+        id="robot"
+        
         onChange = {searchChange}
         /> 
     </div>
